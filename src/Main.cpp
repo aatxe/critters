9190a976e98d3d94fcf7a30c8d9d95d5cf31c5a3
Aaron Weiss <aaronweiss74@gmail.com>
2013-02-24 22:46:38 -0500

    Fixed formatting nonsense.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,19 +1,19 @@
 #include "Global.hpp"
 
 int main() {
-    sf::Window win(sf::VideoMode(800, 600), "Window");
-    while (true) {
-        sf::Event e;
-        while (win.pollEvent(e)) {
-					switch (e.type) {
-        		case sf::Event::Closed:
-            	exit(0);
-            	break;
-						default:
-							break;
-        	}
-				}
-        win.display();
-    }
-    return EXIT_SUCCESS;
+	sf::Window win(sf::VideoMode(800, 600), "Window");
+	while (true) {
+		sf::Event e;
+		while (win.pollEvent(e)) {
+			switch (e.type) {
+				case sf::Event::Closed:
+					exit(0);
+					break;
+				default:
+					break;
+			}
+		}
+		win.display();
+	}
+	return EXIT_SUCCESS;
 }
